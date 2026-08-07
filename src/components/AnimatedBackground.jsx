f9a6f386e58548d45257fff0fb30c665e5f4e180
Harshityadav9838 <harshityadavyadav9838@gmail.com>
2026-08-08 02:47:25 +0530

    Add greeting text to AnimatedBackground component

--- src/components/AnimatedBackground.jsx
+++ src/components/AnimatedBackground.jsx
@@ -3,11 +3,13 @@ import "./AnimatedBackground.css";
 export default function AnimatedBackground() {
   return (
     <div className="animated-bg">
-      <div className="ring red r1"></div>
-      <div className="ring red r2"></div>
-      <div className="ring red r3"></div>
-      <div className="ring red r4"></div>
-      <div className="ring red r5"></div>
+      <div className="test">HELLO BACKGROUND</div>
+
+      <div className="ring r1"></div>
+      <div className="ring r2"></div>
+      <div className="ring r3"></div>
+      <div className="ring r4"></div>
+      <div className="ring r5"></div>
     </div>
   );
 }
