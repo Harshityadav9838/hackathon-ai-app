4daab326b15c6d48f7fbdf2bcb642870f646b08b
Harshityadav9838 <harshityadavyadav9838@gmail.com>
2026-08-08 01:37:21 +0530

    Update AnimatedBackground.jsx

--- src/components/AnimatedBackground.jsx
+++ src/components/AnimatedBackground.jsx
@@ -4,8 +4,6 @@ export default function AnimatedBackground() {
   return (
     <div className="bg">
 
-     
-
       {/* Neon Rings */}
       <div className="ring r1"></div>
       <div className="ring r2"></div>
@@ -13,14 +11,14 @@ export default function AnimatedBackground() {
       <div className="ring r4"></div>
 
       {/* Floating Lights */}
-<div className="light l1"></div>
-<div className="light l2"></div>
-<div className="light l3"></div>
-<div className="light l4"></div>
-<div className="light l5"></div>
-<div className="light l6"></div>
-<div className="light l7"></div>
-<div className="light l8"></div>
+      <div className="light l1"></div>
+      <div className="light l2"></div>
+      <div className="light l3"></div>
+      <div className="light l4"></div>
+      <div className="light l5"></div>
+      <div className="light l6"></div>
+      <div className="light l7"></div>
+      <div className="light l8"></div>
 
       {/* Fog */}
       <div className="fog"></div>
