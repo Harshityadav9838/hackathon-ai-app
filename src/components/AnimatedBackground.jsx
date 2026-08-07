4a58c365e4dc5d55308dd5b55ede2febf55e2b19
Harshityadav9838 <harshityadavyadav9838@gmail.com>
2026-08-08 00:32:02 +0530

    Update comments and class names in AnimatedBackground

--- src/components/AnimatedBackground.jsx
+++ src/components/AnimatedBackground.jsx
@@ -4,13 +4,18 @@ export default function AnimatedBackground() {
   return (
     <div className="bg-container">
 
-      {/* Aurora Lights */}
+      {/* Aurora */}
       <div className="aurora aurora1"></div>
       <div className="aurora aurora2"></div>
       <div className="aurora aurora3"></div>
       <div className="aurora aurora4"></div>
 
-      {/* Vignette */}
+      {/* Neon Rings */}
+      <div className="ring ring1"></div>
+      <div className="ring ring2"></div>
+      <div className="ring ring3"></div>
+
+      {/* Glow */}
       <div className="vignette"></div>
 
     </div>
