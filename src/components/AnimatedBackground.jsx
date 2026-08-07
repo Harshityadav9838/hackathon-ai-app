dd56ad9ae3db28746c429c2af7a908f81cf7f460
Harshityadav9838 <harshityadavyadav9838@gmail.com>
2026-08-08 00:07:25 +0530

    Create AnimatedBackground component
    Add AnimatedBackground component with animated blobs and rings.

--- src/components/AnimatedBackground.jsx
+++ src/components/AnimatedBackground.jsx
@@ -1,1 +1,21 @@
+import "./AnimatedBackground.css";
 
+export default function AnimatedBackground() {
+  return (
+    <div className="bg-container">
+
+      <div className="blob blob1"></div>
+      <div className="blob blob2"></div>
+      <div className="blob blob3"></div>
+      <div className="blob blob4"></div>
+
+      <div className="ring ring1"></div>
+      <div className="ring ring2"></div>
+      <div className="ring ring3"></div>
+      <div className="ring ring4"></div>
+
+      <div className="noise"></div>
+
+    </div>
+  );
+}
