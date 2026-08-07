42fb01765579c4b1ee5706b068720688989f5581
Harshityadav9838 <harshityadavyadav9838@gmail.com>
2026-08-08 02:35:16 +0530

    Update AnimatedBackground component with new design
    Replaced existing background elements with a new animated background design featuring red rings.

--- src/components/AnimatedBackground.jsx
+++ src/components/AnimatedBackground.jsx
@@ -2,30 +2,12 @@ import "./AnimatedBackground.css";
 
 export default function AnimatedBackground() {
   return (
-    <div className="bg">
-
-      {/* Neon Rings */}
-      <div className="ring r1"></div>
-      <div className="ring r2"></div>
-      <div className="ring r3"></div>
-      <div className="ring r4"></div>
-
-      {/* Floating Lights */}
-      <div className="light l1"></div>
-      <div className="light l2"></div>
-      <div className="light l3"></div>
-      <div className="light l4"></div>
-      <div className="light l5"></div>
-      <div className="light l6"></div>
-      <div className="light l7"></div>
-      <div className="light l8"></div>
-
-      {/* Fog */}
-      <div className="fog"></div>
-
-      {/* Vignette */}
-      <div className="vignette"></div>
-
+    <div className="animated-bg">
+      <div className="ring red r1"></div>
+      <div className="ring red r2"></div>
+      <div className="ring red r3"></div>
+      <div className="ring red r4"></div>
+      <div className="ring red r5"></div>
     </div>
   );
 }
